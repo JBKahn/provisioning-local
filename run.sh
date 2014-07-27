2bff348cc40c519e26d5a382b71ced36d9fc49ca
Joseph Kahn <josephbkahn@gmail.com>
2014-07-27 08:02:42 -0400

    fixing up run script

--- run.sh
+++ run.sh
@@ -1,1 +1,23 @@
+sudo apt-get install python-setuptools
+
+sudo easy_install pip
+
+sudo apt-get install aptitude
+sudo apt-get install git
+sudo apt-get install python-dev libxml2-dev libxslt-dev
+
+cd ~
+mkdir setup
+git clone https://github.com/JBKahn/provisioning-local.git
+cd provisioning-local
+
+sudo pip install -r requirements.txt
+
 ansible-playbook setup.yml -i HOSTS --ask-sudo-pass # --module-path ~/ansible/library
+
+dropbox start -i &
+/opt/extras.ubuntu.com/variety/bin/variety $
+plank &
+
+source ~/.bashrc
+exit 0
