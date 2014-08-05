f3fb3fdad0bfd42391a71823e13613fc27a33e0d
Joseph Kahn <josephbkahn@gmail.com>
2014-08-04 23:14:05 -0400

    adding gconftool module and using it for some of my stuff, gotta figure out the boolean stuff

--- run.sh
+++ run.sh
@@ -20,7 +20,11 @@ sudo sed -i "s/^username: .*/username: $PROVISIONING_USER/" roles/common/vars/ma
 echo -e "please enter your github username, followed by [ENTER]" && read PROVISIONING_GITHUB_USERNAME
 sudo sed -i "s/^github_username: .*/github_username: $PROVISIONING_GITHUB_USERNAME/" roles/common/vars/main.yml
 
-ansible-playbook setup.yml -i HOSTS --ask-sudo-pass # --module-path ~/ansible/library
+mkdir -p ~/ansible
+mkdir -p ~/ansible/library
+cp ./gconftool ~/ansible/library/gconftool
+
+ansible-playbook setup.yml -i HOSTS --ask-sudo-pass  --module-path ~/ansible/library
 
 # currently unable to use ansible due to EULA that I can't seem to stub using debconf
 sudo apt-get install steam
